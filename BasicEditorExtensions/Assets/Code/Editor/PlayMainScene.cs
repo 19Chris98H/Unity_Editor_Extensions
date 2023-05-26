92efcda3be86e536a7e0b24e9e057fb95146d3f3
Chris9898 <c.hoerath@gmx.net>
2023-05-26 22:11:35 +0200

    add play main scene

---
+++ BasicEditorExtensions/Assets/Code/Editor/PlayMainScene.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class PlayMainScene : EditorWindow {
+
+    [MenuItem("Play/Play Main Scene %T")]
+    public static void Run() {
+        if (EditorApplication.isPlaying)
+            return;
+        
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+        EditorPrefs.SetString("LastScene", SceneManager.GetActiveScene().path);
+        EditorPrefs.SetBool("ResetToLastScene", true);
+        
+        EditorSceneManager.OpenScene(EditorBuildSettings.scenes[0].path);
+        EditorApplication.isPlaying = true;
+    }
+
+    [InitializeOnLoadMethod]
+    static void Init()
+    {
+        EditorApplication.quitting += () => EditorPrefs.SetBool("ResetToLastScene", false);
+        
+        EditorApplication.playModeStateChanged += (state) =>
+        {
+            if (state == PlayModeStateChange.EnteredEditMode && EditorPrefs.GetBool("ResetToLastScene", false))
+            {
+                EditorPrefs.SetBool("ResetToLastScene", false);
+                EditorSceneManager.OpenScene(EditorPrefs.GetString("LastScene", EditorBuildSettings.scenes[0].path));
+            }
+        };
+    }
+}
