f59c9f8c8637efdc3829e9cdb2b933e663018c74
Chris9898 <c.hoerath@gmx.net>
2023-05-26 22:11:46 +0200

    add clear data

---
+++ BasicEditorExtensions/Assets/Code/Editor/ClearData.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ClearData : EditorWindow
+{
+    [MenuItem("Edit/Clear Data/Clear All")]
+    public static void ClearAll() 
+    {
+        ClearGameSave();
+        ClearOptionSettings();
+    }
+
+    [MenuItem("Edit/Clear Data/Clear Game Save")]
+    public static void ClearGameSave() 
+    {
+        //Adjust the filename for the game data here
+        ClearFile("GameSave");
+    }
+
+    [MenuItem("Edit/Clear Data/Clear Option Settings")]
+    public static void ClearOptionSettings() 
+    {
+        //Adjust the filename for the game settings here
+        ClearFile("OptionSettings");
+    }
+
+    static void ClearFile(string fileName)
+    {
+        try
+        {
+            //Adjust the path here
+            var path = Application.persistentDataPath + "/Data/" + fileName + ".json";
+
+            if(!File.Exists(path))
+            {
+                Debug.LogWarning($"{fileName} already cleared");
+                return;
+            }
+
+            File.Delete(path);
+            Debug.Log($"{fileName} successfully cleared.");          
+        }
+        catch (IOException e)
+        {
+            Debug.LogError($"Something went wrong! {fileName} could not be cleared./nException: {e.Message}");
+        }
+    }
+}
